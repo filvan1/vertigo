63b333e2df15baa1291f8d823e6996eb199eb4f6
Filip Vannfält <filip.vannfalt@gmail.com>
2022-04-07 12:10:43 +0100

    backup before I try to yeet webpack

--- engine/src/renderer/LoadFile.ts
+++ engine/src/renderer/LoadFile.ts
@@ -1,19 +1,30 @@
 //import importBinary from "./sampleGLTF.bin";
-import { Document, Scene, WebIO } from '@gltf-transform/core';
 
-import { createRequire } from "module";
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint8Array#constructor
+import { ReadableStream, toWebReadableStream, toNodeReadable } from "node-web-streams";
+//import { ReadableStream } from "DOM";
+import { Buffer } from "buffer";
 
 export function LoadGLB(fileName: string) {
-	//const binaryFile=require(filePath); https://github.com/KhronosGroup/glTF/blob/main/specification/2.0/figures/gltfOverview-2.0.0b.png
+	//https://developer.mozilla.org/en-US/docs/Web/API/TextEncoder/encodeInto https://github.com/KhronosGroup/glTF/blob/main/specification/2.0/figures/gltfOverview-2.0.0b.png
 
-	import("../../res/" + fileName).then((module) => {
-		var print:Uint8Array =module.default;
+
+	import("../../res/" + fileName).then(async (module) => {
+		var print=module.default;
+		let buff=Buffer.from(print);
+		//let reader=stream.getReader();
+
+		console.log(buff.toString('utf-8',0,4));
+		//const encoder=new TextEncoder();
 		//var data = new DataView(print).getInt32(0, true);
 		
 		/*const outputStr = String.fromCharCode(
 			...print.split(" ").map((bin) => parseInt(bin, 2))
 		);*/
-		console.log(print);
+		//let array:Uint8Array=encoder.encode(print);
+		//console.log(array);
+		
+		//JSON.parse(json);
 	});
 	
 }
